c1d18bff553a4816b96ae1a38b4732914c1dfe87
Nkono <nkono209@gmail.com>
2021-12-10 11:56:58 +0300

    ajout des texts dynamiques

---
+++ src/component/DynamicText.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+
+const DynamicText = () => {
+   useEffect(()=>{
+    let array = ['simply','smart','clean','strong'];
+    let wordIndex = 0;
+    let letterIndex = 0;
+    const target = document.getElementById('text-target');
+    const createLetter = ()=>{
+      const span = document.createElement('span');
+      target.appendChild(span);
+      span.classList.add('letter');
+      span.style.opacity = 0;
+      span.style.animation = "anim 5s ease forwards";
+      span.textContent = array[wordIndex][letterIndex];
+
+      setTimeout(()=>{
+        span.remove();
+      },2000);
+    };
+
+    const loop = () => {
+      setTimeout(()=>{
+        if(wordIndex >= array.length){
+          wordIndex = 0;
+          letterIndex = 0;
+          loop();
+        }else if(letterIndex < array[wordIndex].length){
+          createLetter();
+          letterIndex++;
+          loop();
+        }else{
+          letterIndex = 0;
+          wordIndex++;
+          setTimeout(()=>{
+            loop();
+          },2000)
+        }
+
+      }, 80)
+    }
+    loop();
+   },[]);
+
+  return (
+    <span className="dynamic-text">
+      <span className="simply">simply</span>
+      <span id="text-target"></span>
+    </span>
+  );
+};
+
+export default DynamicText;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Navigation from '../component/Navigation';
-import SocialNetwork from '../component/SocialNetwork';
+import React from "react";
+import DynamicText from "../component/DynamicText";
+import Navigation from "../component/Navigation";
+import SocialNetwork from "../component/SocialNetwork";
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation />
-            <SocialNetwork />
+  return (
+    <div>
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>FS AGENCY</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
